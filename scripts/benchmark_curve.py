f76484358aceac9b532ffaed11521d7494759110
Ryan Aoi <163698864+ryan-aoi@users.noreply.github.com>
2024-12-04 11:34:33 +0900

    fix: time conversion in benchmark (#3)

--- scripts/benchmark_curve.py
+++ scripts/benchmark_curve.py
@@ -29,7 +29,7 @@ def benchmark(fn, text_length, test_num):
 
     logging.info("======== {fn.__name__}({text_length} bytes / {test_num} times) ========")
     t = timeit(run_func, number=test_num)
-    logging.info(f"{fn.__name__}: {t * (1000 ** 2) / test_num:.2f} ns")
+    logging.info(f"{fn.__name__}: {t * 1e9 / test_num:.2f} ns")
 
     return t
 
@@ -80,7 +80,7 @@ def main():
     ]
 
     for fn in hash_functions:
-        time_ns = benchmark_curve(fn, test_length, test_num) * (1000**2)
+        time_ns = benchmark_curve(fn, test_length, test_num) * 1e9
         df_arr.append(pd.DataFrame({"hash": fn.__name__, "length": test_length, "time[ns]": time_ns}))
 
     df = pd.concat(df_arr)
